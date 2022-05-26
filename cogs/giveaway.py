7db6c54a999959ea01e380e60f9fdc6af5ccb0d9
diona feet <tntlauncherpacks@gmail.com>
2022-05-26 21:07:22 +0300

    added persistent giveaways

--- cogs/giveaway.py
+++ cogs/giveaway.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from discord.ui import Select, View
 
-from discord import ApplicationContext, Interaction, OptionChoice, SlashCommandGroup
+from discord import ApplicationContext, ChannelType, Interaction, OptionChoice, SlashCommandGroup, TextChannel
 
 from core import checks
 from core.checks import PermissionLevel
@@ -14,11 +14,15 @@ import time
 
 from threading import Timer
 
+from copy import deepcopy
+
 logger = get_logger(__name__)
 
 
 class Giveaway(commands.Cog):
     _id = "giveaway"
+
+    _guild_id = 849762277041504286
 
     default_cache = {
         "giveaways": {
@@ -46,10 +50,35 @@ class Giveaway(commands.Cog):
         db = await self.db.find_one({"_id": self._id})
         if db is None:
             db = self.default_cache
+        
+        self.cache = db
 
-    def giveaway_end(self, message: Interaction):
+        await self.start_threads()
+
+    async def start_threads(self):
+        cache = deepcopy(self.cache["giveaways"])
+        for message_id in cache:
+            await self.start_thread(int(message_id))
+
+    async def start_thread(self, message_id):
+        guild: discord.Guild = self.bot.get_guild(self._guild_id)
+        channel: TextChannel = guild.get_channel(self.cache["giveaways"][str(message_id)]["channel"])
+        message = await channel.fetch_message(message_id)
+
+        unix = self.cache["giveaways"][str(message_id)]["unixTime"] - int(time.mktime(datetime.now().timetuple()))
+
+        th = Timer(unix, self.giveaway_end, {message})
+        th.start()
+
+    def giveaway_end(self, message: discord.Message):
+        if message == None:
+            return
+
         embed = discord.Embed(title="Giveaway", description="Giveaway ended")
-        self.bot.loop.create_task(message.edit_original_message(embed=embed))
+        self.bot.loop.create_task(message.edit(embed=embed))
+
+        self.cache["giveaways"].pop(str(message.id))
+        self.bot.loop.create_task(self.update_db())
 
     @_ga.command(name="create", description="Creates a new giveaway")
     @checks.has_permissions(PermissionLevel.OWNER)
@@ -59,10 +88,18 @@ class Giveaway(commands.Cog):
 
         embed = discord.Embed(
             title="Giveaway", description=f"Giveaway started and will end at <t:{unix_now + offset}:F>")
-        message = await ctx.respond(embed=embed)
+        interaction = await ctx.respond(embed=embed)
+        message = await interaction.original_message()
 
-        th = Timer(offset, self.giveaway_end, {message})
-        th.start()
+        giveaway = {
+            "channel": ctx.channel_id,
+            "unixTime": unix_now + offset
+        }
+
+        self.cache["giveaways"].update({str(message.id): giveaway})
+        await self.update_db()
+
+        await self.start_thread(message.id)
 
 
 def setup(bot):
