285a63bc6a55a1707898f8e1eb2bda27f4f490de
diona feet <tntlauncherpacks@gmail.com>
2022-06-07 06:03:27 +0300

    updated context

--- core/context.py
+++ core/context.py
@@ -1,16 +1,16 @@
 
 
-
-
-class Context():
-    def __init__(self, member, moderator, timestamp, reason = None, duration=None, id = None):
+class ModContext():
+    def __init__(self, member, moderator, timestamp, reason=None, duration=None, id=None):
         self.member = member
         self.moderator = moderator
         self.timestamp = timestamp
+
         if duration:
             self.duration = duration
+
         if reason:
             self.reason = reason
+
         if id:
             self.id = id
-
