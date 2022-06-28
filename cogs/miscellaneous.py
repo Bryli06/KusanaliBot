0d41b17db1f85ba2bb86c27fc8b8a9f47e9ee6c7
Bryan Li <bryan@Jianmings-MacBook-Pro.local>
2022-06-28 14:02:57 -0700

    added idp

--- cogs/miscellaneous.py
+++ cogs/miscellaneous.py
@@ -23,5 +23,31 @@ class Miscellaneous(BaseCog):
 
         await ctx.respond(embed=embed)
 
+
+    @commands.slash_command(name="idp", description="It depends")
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    async def idp(self, ctx: ApplicationContext):
+        description = ("*Depends on characters*\n"
+                    "*Depends on constellations*\n"
+                    "*Depends on weapons*\n"
+                    "*Depends on substats*\n"
+                    "*Depends on team comps*\n"
+                    "*Depends on your PC*\n"
+                    "*Depends on your Wifi*\n"
+                    "*Depends on the current moon phase*\n"
+                    "*Depends on the brand of cereal Biden ate today*\n"
+                    "*Depends on the number of chocolate chips in Liu Wei’s cookie*\n"
+                    "*Depends on the moles of oxygen in the air*\n"
+                    "*Depends on your distance from a blackhole*\n"
+                    "*Depends on the number of hairs you have over 2.21 mm long*\n"
+                    "*Depends on the hydration levels in the pee of a guy named Darmo Kasim in Indonesia*\n"
+                    "*Depends on if P = NP*\n"
+                    "*Depends on if Drak can finally get a job*\n\n"
+                    "**It Depends™**")
+        embed = discord.Embed(title="It Depends", description=description, colour=Colour.red())
+
+        await ctx.respond(embed=embed)
+
+
 def setup(bot):
     bot.add_cog(Miscellaneous(bot))
